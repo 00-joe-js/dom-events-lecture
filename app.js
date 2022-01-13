91a598b6ac143bdf9c379b6b1f867398145d02f9
Joe Alves <joseph.michael.alves@gmail.com>
2022-01-13 10:58:28 -0500

    Latest code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const h1Element = document.querySelector("#message");
+const mainElement = document.querySelector("main");
+const sectionElement = document.querySelector("section");
 
 const createAnotherElement = (whatItShouldSay) => {
   const newH2 = document.createElement("h2");
@@ -6,16 +8,41 @@ const createAnotherElement = (whatItShouldSay) => {
   document.body.appendChild(newH2);
 };
 
-const myClickListener = function (eventDetails) {
-  console.log(eventDetails.target);
+h1Element.addEventListener("click", function (eventDetails) {
+  eventDetails.stopPropagation();
   if (eventDetails.x < 150) {
     createAnotherElement("Hi Violet");
   } else {
     createAnotherElement("Hi Joe");
   }
-}
+});
 
-h1Element.addEventListener("click", myClickListener);
+h1Element.addEventListener("click", function (eventDetails) {
+  console.log("second listener for h1");
+});
+
+sectionElement.addEventListener("click", function (e) {
+  e.stopPropagation();
+  console.log("SECTION ELEMENT LOG");
+});
+
+mainElement.addEventListener("click", function (eventObject) {
+  eventObject.stopPropagation();
+  console.log("MAIN ELEMENT LOG");
+});
+
+document.body.addEventListener("click", function (banana) {
+  createAnotherElement("You missed!");
+});
+
+window.addEventListener("keydown", (keyboardEvent) => {
+  createAnotherElement("you typed on the body!");
+  console.log("typed something!");
+});
+
+document.querySelector("#text-input").addEventListener("keydown", (e) => {
+  e.stopPropagation();
+});
 
 // h1Element.addEventListener("mousemove", function (mouseMoveEvent) {
 //   console.log(mouseMoveEvent.x, mouseMoveEvent.y);
