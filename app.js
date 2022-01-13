ab395549f6c36e88830e092c37c20768cbd95cae
Joe Alves <joseph.michael.alves@gmail.com>
2022-01-13 10:30:53 -0500

    A bit more

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const h1Element = document.querySelector("#message");
 
-const listener = function (eventDetails) {
-    console.log(2+2);
- }
+const createAnotherElement = (whatItShouldSay) => {
+  const newH2 = document.createElement("h2");
+  newH2.innerText = whatItShouldSay;
+  document.body.appendChild(newH2);
+};
 
-window.addEventListener("scroll", () => {
-    console.log("scrolling")
-})
+const myClickListener = function (eventDetails) {
+  console.log(eventDetails.target);
+  if (eventDetails.x < 150) {
+    createAnotherElement("Hi Violet");
+  } else {
+    createAnotherElement("Hi Joe");
+  }
+}
 
-h1Element.addEventListener("click", listener);
+h1Element.addEventListener("click", myClickListener);
 
-h1Element.addEventListener("mouseenter", function () {
-    console.log("i'm in here!")
-});
+// h1Element.addEventListener("mousemove", function (mouseMoveEvent) {
+//   console.log(mouseMoveEvent.x, mouseMoveEvent.y);
+// });
 
-h1Element.addEventListener("mouseleave", function () {
-    console.log("I have left!")
-});
+// h1Element.addEventListener("mouseenter", function () {
+//   console.log("i'm in here!");
+// });
+
+// h1Element.addEventListener("mouseleave", function () {
+//   console.log("I have left!");
+// });
